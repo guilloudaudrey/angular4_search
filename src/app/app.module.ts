dd1c9f6ef6ddc0aa900d4762cf5c9402048ed11b
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-10-05 14:32:53 +0200

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
 import { SearchUsersComponent } from './search-users/search-users.component';
-
+import { SearchUsersService } from './search-users.service';
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +16,7 @@ import { SearchUsersComponent } from './search-users/search-users.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ SearchUsersService ], // It is important
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/search-users/search-users.component.ts
+++ src/app/search-users/search-users.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SearchUsersService } from '../search-users.service';
 
 @Component({
   selector: 'app-search-users',
@@ -6,18 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-users.component.css']
 })
 export class SearchUsersComponent implements OnInit {
-  place:string;
-  language:string;
+  place: string;
+  language: string;
 
-  constructor() { }
+  results: any[] = []; // This will hold the data coming from the service
+  selected: boolean = false; // Flag to check if a user is clicked or not
+  selectedUser: any; // presently Selected user details
+  error_text: string = ""; // So called error reporing text to the end user
 
-  ngOnInit() {
+  constructor(private serachService: SearchUsersService) {}
+  ngOnInit() {}
+
+ 
+  search(place: string, language: string) {
+    this.selected = false;
+    this.error_text = "";
+    if (place || language) {
+      this.place = place;
+      this.language = language;
+      this.serachService.getUsersByPlaceAndLanguage(place, language).subscribe(
+        users => {
+          this.results = users;
+        },
+        error => {
+          this.results = [];
+          this.error_text = "Sorry! No Users found. Try again";
+          console.error(error);
+        }
+      )
+    }
   }
-
-  search(place:string, language:string){
-  this.place = place;
-  this.language = language;
-  console.log(this.place, this.language)
-  }
-
 }
