5492b925d52869770815f7458608dc28bf401e9b
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-10-05 12:20:12 +0200

    search boxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { SearchUsersComponent } from './search-users/search-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchUsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search-users/search-users.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-users',
+  templateUrl: './search-users.component.html',
+  styleUrls: ['./search-users.component.css']
+})
+export class SearchUsersComponent implements OnInit {
+  place:string;
+  language:string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  search(place:string, language:string){
+  this.place = place;
+  this.language = language;
+  console.log(this.place, this.language)
+  }
+
+}
